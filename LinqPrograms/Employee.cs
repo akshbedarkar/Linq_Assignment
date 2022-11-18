676365d1c9dc0c47d03fad9f6bb4b3e736d4738c
akshbedarkar <114993159+akshbedarkar@users.noreply.github.com>
2022-11-18 19:39:28 +0530

    Linq Assignment

---
+++ LinqPrograms/Employee.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPrograms
+{
+    internal class Employee
+    {
+        #region Variables
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public DateTime NoteDate { get; set; }
+        
+
+        public DateTime DOJ { get; set; }
+
+        public string City { get; set; }
+
+
+
+        #endregion
+
+        
+
+
+        public static List<Employee> GetAllDetails()
+        {
+            List<Employee> listemployees = new List<Employee>
+            {
+                new Employee
+                {
+                    Id = 1001, 
+                    FirstName = "Malcolm", 
+                    LastName = "Daruwalla", 
+                    Title = "Manager",
+                    NoteDate=new DateTime(1984,11,16),
+                    DOJ= new DateTime(2011,08,06),
+                    City="Mumbai",
+                },
+
+                new Employee
+                {
+                    Id = 1002, FirstName = "Asdin", LastName = "Dhalla", Title = "AsstManager", NoteDate=new DateTime(1984,11,16),DOJ= new DateTime(2011,06,08),City= "Mumbai",
+
+                },
+                new Employee
+                {
+                    Id = 1003, FirstName = "Madhvi", LastName = "Oza", Title = "Consultant",NoteDate=new DateTime(1984,08,20),DOJ= new DateTime(2012,07,07),City= "Pune",
+                },
+                new Employee
+                {
+                    Id = 1004, FirstName = "Saba", LastName = "Shaikh", Title = "SE", NoteDate=new DateTime(1987,11,14),DOJ= new DateTime(2015,04,12),City= "Pune",
+                },
+                new Employee
+                {
+                    Id = 1005, FirstName = "Nazia", LastName = "Shaikh", Title = "SE", NoteDate=new DateTime(1990,06,03),DOJ= new DateTime(2016,02,02),City= "Mumbai",
+
+                },new Employee
+                {
+                    Id = 1006, FirstName = "Amit", LastName = "Pathak", Title = "Consultant",NoteDate=new DateTime(1991,03,08),DOJ= new DateTime(2016,02,02),City="Chennai",
+                },
+                new Employee
+                {
+                    Id = 1007, FirstName = "Vijay", LastName = "Natrajan", Title = "Consultant", NoteDate=new DateTime(1989,12,02),DOJ= new DateTime(2015,06,01),City= "Mumbai",
+                },new Employee
+                {
+                    Id = 1008, FirstName = "Rahul", LastName = "Dubey", Title = "Associate",NoteDate=new DateTime(1993,11,11),DOJ= new DateTime(2014,11,06),City= "Chennai",
+                },
+                new Employee
+                {
+                    Id = 1009, FirstName = "Suresh", LastName = "Mistry", Title = "Associate",NoteDate=new DateTime(1992,08,12),DOJ= new DateTime(2014,12,03),City= "Chennai",
+
+                },new Employee
+                {
+                    Id = 1010, FirstName = "Suresh", LastName = "Mistry", Title = "Associate", NoteDate=new DateTime(1991,04,12),DOJ= new DateTime(2016,01,02),City= "Chennai",
+                },
+               
+
+            };
+            return listemployees;
+
+        }
+}
+    
+}
---
+++ LinqPrograms/Program.cs
@@ -0,0 +1,234 @@
+﻿using System.Linq;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace LinqPrograms
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("1.select everthing from employee");
+            //var employeegroup = from employee in Employee.GetAllDetails()
+            //                    select employee;
+
+
+
+
+
+
+            //Console.WriteLine("the employee details are :");
+            //foreach (Employee emp in employeegroup)
+            //{
+
+            //    Console.WriteLine("employee id :{0} employee firstname :{1} employee lastname :{2} title :{3} date : {4}, doj :{5} ,city:{5} ", emp.Id, emp.FirstName, emp.LastName, emp.Title, emp.NoteDate, emp.DOJ, emp.City);
+            //    Console.WriteLine("-----------------------------");
+            //}
+
+
+
+
+
+
+            //Console.WriteLine("b. Display details of all Employee whose location is not Mumbai.");
+            //var employeecity = from emp in Employee.GetAllDetails() where emp.City != "Mumbai" select emp;
+            //foreach (Employee e1 in employeecity)
+            //{
+            //    Console.WriteLine(e1.City);
+            //}
+            //Console.WriteLine("--------------------------------------");
+
+
+
+
+
+
+
+            //Console.WriteLine("b. display employee whose title is AsstManager");
+            //var employeetitle = from emp in Employee.GetAllDetails() where emp.Title == "AsstManager" select emp;
+            //foreach (Employee e1 in employeetitle)
+            //{
+            //    Console.WriteLine(e1.Title);
+            //}
+            //Console.WriteLine("--------------------------------------");
+
+
+
+
+
+
+
+
+            //Console.WriteLine("display list of employee whose last name starts with S");
+            //var employeename = from emp in Employee.GetAllDetails() where emp.LastName.StartsWith("S") select emp;
+
+            //foreach(Employee emp in employeename)
+            //{
+            //    Console.WriteLine(emp.LastName);
+            //}
+
+
+
+
+
+
+
+            //Console.WriteLine("display the list of all employees whose designation is Consultant and Asssociate");
+            //var emptitle = from emp in Employee.GetAllDetails().Where(emp => emp.Title == "Consultant" || (emp.Title == "Asssociate")) select emp;
+
+            //foreach (Employee emp in emptitle)
+            //{
+            //    Console.WriteLine(emp.Title);
+            //}
+
+
+
+
+
+
+            //Console.WriteLine("display the total number of employees");
+            //int employeecount = Employee.GetAllDetails().Where(x => x.Id != null).Count();
+
+            //Console.WriteLine(employeecount);
+
+
+
+
+            //Console.WriteLine("display the total number of employees belonging to chennai");
+            //int employeecount = Employee.GetAllDetails().Where(x => x.City.Contains("Chennai")).Count();
+
+            //Console.WriteLine(employeecount);
+
+
+
+
+
+
+            //Console.WriteLine("display the highest employee id");
+            //int employeecount  = (from emp  in Employee.GetAllDetails() select emp.Id).Max();
+            //Console.WriteLine(employeecount);
+
+
+
+
+
+            //Console.WriteLine("display the list of all employees whose designation is not Associate");
+            //var emptitle = from emp in Employee.GetAllDetails().Where(emp => emp.Title != "Associate") select emp;
+
+            //foreach (Employee emp in emptitle)
+            //{
+            //    Console.WriteLine(emp.Title);
+            //}
+
+
+
+
+
+
+            //Console.WriteLine("Group employee based on city");
+            //var employeecity = from emp in Employee.GetAllDetails()
+            //                   group emp by emp.City;
+
+            //foreach(var emp in employeecity)
+            //{
+            //    Console.WriteLine("{0} = {1}",emp.Key,emp.Count());
+            //}
+
+
+
+
+
+            //Console.WriteLine("n. display total number of employee based on city and Tittle");
+            //var employeecity = from emp in Employee.GetAllDetails()
+            //          group emp by new { emp.City, emp.Title } into egroup
+            //          orderby egroup.Key.City
+            //          select new { City = egroup.Key.City, Title = egroup.Key.Title, Count = egroup.Count() };
+            //foreach (var emp in employeecity)
+            //{
+
+            //        Console.WriteLine("Employee city:{0}  Employee Title{1}   Employeecount{2}", emp.City, emp.Title, emp.Count);
+
+            //}
+
+
+            //var startdate = new DateTime(2015, 1, 1);
+            //Console.WriteLine("display the list of employee whose  have joined before 1/1/2015");
+            //var employeedoj = from emp in Employee.GetAllDetails().Where(x => x.DOJ > startdate)
+            //                  select emp;
+
+
+            //foreach (var emp in employeedoj)
+            //{
+
+            //    Console.WriteLine(emp.DOJ);
+
+            //}
+
+
+
+            //var dateofbirth = new DateTime(1990, 1, 1);
+            //Console.WriteLine("display the list of employee whose date of birth is 1990,1,1");
+            //var employeedoj = from emp in Employee.GetAllDetails().Where(x => x.NoteDate > dateofbirth)
+            //                  select emp;
+
+
+            //foreach (var emp in employeedoj)
+            //{
+
+            //    Console.WriteLine(emp.NoteDate);
+
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
